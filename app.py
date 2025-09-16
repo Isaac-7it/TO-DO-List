237b515ac5b0c94d7585207573cc7cae3142a910
dev AIO <isaacmail.07@gmail.com>
2025-09-16 12:16:56 +0100

    commiting project files

---
+++ app.py
@@ -0,0 +1,34 @@
+from itertools import count
+
+running = True
+tasks = []
+
+print('''---TO-DO LIST---
+1. Add Task
+2. View Task
+3. Remove Task
+4. Exit''')
+while running:
+    command = input('>>> Choose an option: ')
+    if command == '1' or command == '1.' or command.lower() == 'add':
+        task = input('>>> Enter a task: ').lower()
+        if task != '' and task not in tasks:
+            tasks.append(task)
+            print('>>> Task successfully added!')
+        elif task in tasks:
+            print('>>> Task has been added earlier!')
+    elif command == '2' or command == '2.' or command.lower() == 'view':
+        if len(tasks) != 0:
+            counter = 1
+            print('Your task(s) are:')
+            for task in tasks:
+                print(f'{counter}. {task.capitalize()}')
+                counter += 1
+        else:
+            print(f">>> Nothing to show! Use the 'add' command to add task(s)")
+    elif command == '3' or command == '3.' or command.lower() == 'remove':
+        task = input('Enter the task to remove: ').lower()
+        tasks.remove(task)
+    elif command == '4' or command == '4.' or command.lower() == 'exit':
+        print('Goodbye!')
+        break
